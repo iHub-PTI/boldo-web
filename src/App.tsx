3e7d887cc4a7073ca64a36fda4452d3db1dd0d2a
Björn Schmidtke <bjoern@penguin.digital>
2020-10-16 03:52:31 -0300

    improve routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, createContext, useState } from 'react'
 import axios from 'axios'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 
 import Call from './pages/Call'
 import Dashboard from './pages/Dashboard'
@@ -67,15 +67,19 @@ const App = () => {
       <div className='antialiased App'>
         <Switch>
           <Route exact path='/'>
-            <Home />
+            <Dashboard />
           </Route>
 
           <Route exact path='/call'>
             <Call />
           </Route>
 
-          <Route exact path='/dashboard'>
-            <Dashboard />
+          <Route exact path='/home'>
+            <Home />
+          </Route>
+
+          <Route>
+            <Redirect to='/' />
           </Route>
         </Switch>
       </div>
