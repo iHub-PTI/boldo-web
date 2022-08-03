d24a195084297d7275855828c1f6e4c280332403
sebasrival <sebasrival@gmail.com>
2022-08-03 00:47:38 -0400

    style: add studies load indicator

--- src/components/LaboratoryMenu.tsx
+++ src/components/LaboratoryMenu.tsx
@@ -32,6 +32,7 @@ export function LaboratoryMenu(props) {
     const [showEditModal, setShowEditModal] = useState(false)
     const [showPreview, setShowPreview] = useState({})
     const [categorySelect, setCategory] = useState("")
+    const [loadPreview, setLoadPreview] = useState(false)
 
     const tableIcons: Icons = {
         SortArrow: forwardRef((props, ref) => <ArrowUpward style={{ color: "#13A5A9" }} {...props} ref={ref} />),
@@ -61,6 +62,7 @@ export function LaboratoryMenu(props) {
     }, [appointment])
     const downloadBlob = (url, contentType, download) => {
         var oReq = new XMLHttpRequest();
+        setLoadPreview(true) //loading preview data modal
         oReq.open("GET", url, true);
         oReq.responseType = "blob";
         oReq.onload = function () {
@@ -82,10 +84,10 @@ export function LaboratoryMenu(props) {
                 //showPreview
                 setShowEditModal(true)
                 setShowPreview({ contentType: contentType, url: fileURL })
+                setLoadPreview(false)
             }
-
         };
-
+        
         oReq.send();
 
     }
@@ -580,8 +582,27 @@ export function LaboratoryMenu(props) {
                 </div>
 
                 {
-                    showPreview['contentType'] !== undefined && showPreview['contentType'].includes("pdf") ? <object data={showPreview['url']} width="700" height="700" type="application/pdf"></object> : <img src={showPreview['url']} alt="img" />
+                    !loadPreview ? (showPreview['contentType'] !== undefined && showPreview['contentType'].includes("pdf") ? <object data={showPreview['url']} width="700" height="700" type="application/pdf"></object> : <img src={showPreview['url']} alt="img" />) : (
+                        <div style={{ width: '300px', margin: 'auto' }} className='flex items-center justify-center w-full h-full py-64'>
+                            <div className='flex items-center justify-center w-12 h-12 mx-auto bg-gray-100 rounded-full'>
+                                <svg
+                                    className='w-6 h-6 text-secondary-500 animate-spin'
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    fill='none'
+                                    viewBox='0 0 24 24'
+                                >
+                                    <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='2'></circle>
+                                    <path
+                                        className='opacity-75'
+                                        fill='currentColor'
+                                        d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+                                    ></path>
+                                </svg>
+                            </div>
+                        </div>
+                    )
                 }
+                
                 
             </Modal>
 
