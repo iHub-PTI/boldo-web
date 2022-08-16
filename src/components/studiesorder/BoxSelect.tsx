1262b4458c6534350b57526767d64d012da41235
sebasrival <sebasrival@gmail.com>
2022-08-16 15:21:12 -0400

    add template icon svg

--- src/components/studiesorder/BoxSelect.tsx
+++ src/components/studiesorder/BoxSelect.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import SelectItem from './SelectItem'
+import {ReactComponent as TemplateStudy } from '../../assets/template-study.svg'
 
 const BoxSelect = ({ options }) => {
 
@@ -14,6 +15,9 @@ const BoxSelect = ({ options }) => {
     return (
         <div className="flex flex-row flex-wrap bg-white border border-gray-300 rounded items-center"
         style={{minHeight:'3rem'}}>
+            <div className="m-1 p-2 w-auto hover:bg-primary-200 cursor-pointer rounded-full">
+                <TemplateStudy></TemplateStudy>
+            </div>
             {data && data.map( (item) => {
                 return <SelectItem id={item.id} value={item.name} handleDelete={deleteData}></SelectItem>
             })}
