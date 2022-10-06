ecb4582ecc0d4a1fbc5739bc76cf8cc7c5ba49e6
sebasrival <sebasrival@gmail.com>
2022-10-06 14:35:08 -0300

    fix material table

--- src/pages/Call.tsx
+++ src/pages/Call.tsx
@@ -48,6 +48,23 @@ import {
   withStyles,
 } from '@material-ui/core'
 import Modal from '../components/Modal'
+import { Icons } from 'material-table';
+import { forwardRef } from 'react';
+import ArrowUpward from '@material-ui/icons/ArrowUpward';
+import AddBox from '@material-ui/icons/AddBox';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 import MaterialTable from 'material-table'
 import PrivateComments from '../components/PrivateComments'
 import CancelAppointmentModal from '../components/CancelAppointmentModal'
@@ -1109,6 +1126,7 @@ const useStyles = makeStyles(theme => ({
   },
 }))
 
+
 function SOEP({ appointment }: { appointment: any }) {
   const [value, setValue] = useState(0)
   const [mainReason, setMainReason] = useState('')
@@ -1139,6 +1157,26 @@ function SOEP({ appointment }: { appointment: any }) {
   const { addErrorToast, addToast } = useToasts()
   let match = useRouteMatch<{ id: string }>('/appointments/:id/call')
   const id = match?.params.id
+
+  const tableIcons: Icons = {
+    SortArrow: forwardRef((props, ref) => <ArrowUpward style={{ color: "#13A5A9" }} {...props} ref={ref} />),
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  }
 
   useEffect(() => {
     const load = async () => {
@@ -1920,13 +1958,37 @@ function SOEP({ appointment }: { appointment: any }) {
                   </Typography>
 
                   <MaterialTable
+                    title='Seleccionar seguimiento'
+                    icons={tableIcons}
+                    localization={{
+                      body: {
+                        emptyDataSourceMessage: 'No hay datos por mostrar',
+                      },
+                      pagination: {
+                        firstAriaLabel: 'Primera página',
+                        firstTooltip: 'Primera página',
+                        labelDisplayedRows: '{from}-{to} de {count}',
+                        labelRowsPerPage: 'Filas por página:',
+                        labelRowsSelect: 'filas',
+                        lastAriaLabel: 'Ultima página',
+                        lastTooltip: 'Ultima página',
+                        nextAriaLabel: 'Pagina siguiente',
+                        nextTooltip: 'Pagina siguiente',
+                        previousAriaLabel: 'Pagina anterior',
+                        previousTooltip: 'Pagina anterior',
+                     },
+                     toolbar: {
+                      searchPlaceholder: 'Buscar',
+                      searchTooltip: 'Buscar',
+                    },
+                    }}
                     columns={[
                       {
                         title: 'Fecha',
                         field: 'startTimeDate',
                       },
                       {
-                        title: 'motivo de visita',
+                        title: 'Motivo de visita',
                         field: 'mainReason',
                       },
                       // {
@@ -1955,12 +2017,11 @@ function SOEP({ appointment }: { appointment: any }) {
                       setSelectedRow(selectedRow)
                     }
                     options={{
-                      search: false,
-
-                      toolbar: false,
-                      paging: false,
+                      search: true,
+                      toolbar: true,
+                      paging: true,
                       draggable: false,
-
+                      pageSize: 5,
                       rowStyle: rowData => ({
                         backgroundColor:
                           // @ts-ignore
