c6280f7033dea7c7b7a314644f52dc01273bde4e
sebasrival <sebasrival@gmail.com>
2022-10-07 10:17:52 -0300

    test update

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -3,16 +3,21 @@ import React, { useEffect } from 'react'
 export const Download = () => {
   let device = navigator.userAgent
   if (device.match(/Iphone/i) || device.match(/Ipod/i) || device.match(/iPhone|iPad|iPod/i)) {
-    window.location.href = 'https://apps.apple.com/us/app/boldo/id1614655648' //URL del APP Store
+    //window.location.href = 'https://apps.apple.com/us/app/boldo/id1614655648' //URL del APP Store
   } else {
     if (device.match(/Android/i)) {
-      window.location.href = 'https://play.google.com/store/apps/details?id=py.org.pti.boldo' //URL del Play Store
+      
+      //window.location.href = 'https://play.google.com/store/apps/details?id=py.org.pti.boldo' //URL del Play Store
     } else {
-      window.location.href = 'https://www.pti.org.py/' //En el caso que el cliente este visitando desde un computador, lo enviamos a la web.
+      //window.location.href = 'https://www.pti.org.py/' //En el caso que el cliente este visitando desde un computador, lo enviamos a la web.
     }
   }
   useEffect(()=>{
     document.title = 'Boldo para Pacientes'
+    let metaDescripcion = document.head.querySelector('meta[name="description"]')
+    let metaName = document.head.querySelector('meta[name="application-name"]')
+    metaDescripcion['content'] = 'Boldo para Pacientes - Telemedicina en Paraguay'
+    metaName['content'] = 'Boldo para Pacientes'
   },[])
   return <></>
 }
