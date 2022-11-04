b2dc19656243e87f7b31f41965c020a07e8ee81d
sebasrival <sebasrival@gmail.com>
2022-11-04 10:47:09 -0300

    capture exception with puglins integrations

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import * as Sentry from '@sentry/react'
-import { Integrations } from '@sentry/tracing'
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+import { RewriteFrames as RewriteFramesIntegration } from "@sentry/integrations";
+import { Debug as DebugIntegration } from "@sentry/integrations";
+import { ExtraErrorData as ExtraErrorDataIntegration } from "@sentry/integrations";
+import { CaptureConsole as CaptureConsoleIntegration } from "@sentry/integrations";
+import { ReportingObserver as ReportingObserverIntegration } from "@sentry/integrations";
+
 import {Provider} from 'react-redux'
 import {createStore} from 'redux'
 import App from './App'
 import soepReducer from './redux/reducers/soepReducer.js';
 if (process.env.NODE_ENV === 'production')
   Sentry.init({
-    dsn: process.env.REACT_APP_SENTRY,
-    autoSessionTracking: true,
-    integrations: [new Integrations.BrowserTracing()],
-    tracesSampleRate: 1.0,
+    dsn: "https://7d847dd3f51d4c8a9b99913cffece274@o1301148.ingest.sentry.io/4504090880573440",
+    integrations: [new Integrations.BrowserTracing(),
+      new RewriteFramesIntegration(),
+      new DebugIntegration(),
+      new ExtraErrorDataIntegration(),
+      new CaptureConsoleIntegration(
+        {
+          levels: ['error']
+        }
+      ),
+      new ReportingObserverIntegration()],
   })
 
-  const configureStore = createStore(soepReducer);
+const configureStore = createStore(soepReducer);
+
 ReactDOM.render(
   <Provider store={configureStore}>
    <BrowserRouter>
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -33,11 +33,7 @@ export const upload = async (file: File | string) => {
     })
     if (ress.status === 201) return res.data.location
   } catch (err) {
-    Sentry.withScope(function(scope) {
-      // group errors together based on their request and response
-      scope.setFingerprint(['get', '/presigned', String(err.response?.data.message)]);
-      Sentry.captureException(err);
-    });
+    Sentry.captureException(err)
     console.log(err)
   }
 }
@@ -174,11 +170,7 @@ const Settings = (props: Props) => {
         }
       } catch (err) {
         console.log(err)
-        Sentry.withScope(function(scope) {
-          // group errors together based on their request and response
-          scope.setFingerprint(['get', '/profile/doctor', String(err.response?.data.message)]);
-          Sentry.captureException(err);
-        });
+        Sentry.captureException(err)
         if (mounted) {
           setError('ERROR: Fallo en la carga de datos iniciales')
           setShow(true)
@@ -240,11 +232,7 @@ const Settings = (props: Props) => {
           new: false,
         })
       } catch (err) {
-        Sentry.withScope(function(scope) {
-          // group errors together based on their request and response
-          scope.setFingerprint(['post', '/profile/doctor', String(err.response?.data.message)]);
-          Sentry.captureException(err);
-        });
+        Sentry.captureException(err)
         setError(err.response?.data.message || 'Ha ocurrido un error! Intente de nuevo.')
         console.log(err)
       }
@@ -671,7 +659,7 @@ const Settings = (props: Props) => {
   )
 }
 
-export default Settings
+export default Sentry.withProfiler(Settings) 
 
 interface SaveButtonProps {
   error?: string
