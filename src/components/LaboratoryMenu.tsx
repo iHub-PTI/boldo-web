bf22e7d1634dbd710c5a46bf5092d0715eca526d
elias7991 <eliascaceres124@gmail.com>
2022-11-23 15:38:54 -0300

    fix: control for face to face locked status

--- src/components/LaboratoryMenu.tsx
+++ src/components/LaboratoryMenu.tsx
@@ -248,7 +248,15 @@ export function LaboratoryMenu(props) {
                               archivos subidos por el paciente, laboratorios o dispositivos médicos
                           </Typography>
                       </Grid>
-                      <div title={disabledButton ? 'La gestión de órdenes se habilitará ' + TIME_TO_OPEN_APPOINTMENT + ' minutos antes del inicio de la cita' : 'Aquí puede gestionar las órdenes de estudio y emitirlas'}>
+                      <div 
+                        title={
+                          disabledButton  
+                            ? appointment.status === 'locked'
+                              ? 'No disponible en citas culminadas'
+                              : 'La gestión de órdenes se habilitará ' + TIME_TO_OPEN_APPOINTMENT + ' minutos antes del inicio de la cita' 
+                            : 'Aquí puede gestionar las órdenes de estudio y emitirlas'
+                        }
+                      >
                         <button className={`btn ${disabledButton ? 'bg-gray-200 cursor-not-allowed': 'bg-primary-600'} text-white border-transparent focus:outline-none flex flex-row justify-end items-center px-2 py-0 h-10 rounded-l-3xl rounded-r-3xl text-clip md-max:mt-2`}
                           onClick={() => setShowMakeOrder(true)}
                           disabled={disabledButton}
