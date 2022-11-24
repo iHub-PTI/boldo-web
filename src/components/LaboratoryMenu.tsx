15a129d4f6758062273729712c4a40d012733393
elias7991 <eliascaceres124@gmail.com>
2022-11-24 11:24:32 -0300

    fix: study order button when appointment loaded

--- src/components/LaboratoryMenu.tsx
+++ src/components/LaboratoryMenu.tsx
@@ -248,23 +248,25 @@ export function LaboratoryMenu(props) {
                               archivos subidos por el paciente, laboratorios o dispositivos médicos
                           </Typography>
                       </Grid>
-                      <div 
-                        title={
-                          disabledButton  
-                            ? appointment.status === 'locked'
-                              ? 'No disponible en citas culminadas'
-                              : 'La gestión de órdenes se habilitará ' + TIME_TO_OPEN_APPOINTMENT + ' minutos antes del inicio de la cita' 
-                            : 'Aquí puede gestionar las órdenes de estudio y emitirlas'
-                        }
-                      >
-                        <button className={`btn ${disabledButton ? 'bg-gray-200 cursor-not-allowed': 'bg-primary-600'} text-white border-transparent focus:outline-none flex flex-row justify-end items-center px-2 py-0 h-10 rounded-l-3xl rounded-r-3xl text-clip md-max:mt-2`}
-                          onClick={() => setShowMakeOrder(true)}
-                          disabled={disabledButton}
+                      {appointment && 
+                        <div 
+                          title={
+                            disabledButton  
+                              ? appointment.status === 'locked'
+                                ? 'No disponible en citas culminadas'
+                                : 'La gestión de órdenes se habilitará ' + TIME_TO_OPEN_APPOINTMENT + ' minutos antes del inicio de la cita' 
+                              : 'Aquí puede gestionar las órdenes de estudio y emitirlas'
+                          }
                         >
-                            <div>Emitir orden de estudio</div>
-                            <OrderAdd className="mx-0.5 p-0 "></OrderAdd>
-                        </button>
-                      </div>
+                          <button className={`btn ${disabledButton ? 'bg-gray-200 cursor-not-allowed': 'bg-primary-600'} text-white border-transparent focus:outline-none flex flex-row justify-end items-center px-2 py-0 h-10 rounded-l-3xl rounded-r-3xl text-clip md-max:mt-2`}
+                            onClick={() => setShowMakeOrder(true)}
+                            disabled={disabledButton}
+                          >
+                              <div>Emitir orden de estudio</div>
+                              <OrderAdd className="mx-0.5 p-0 "></OrderAdd>
+                          </button>
+                        </div>
+                      }
                   </div>
                   <div className="flex flex-row flex-no-wrap w-full justify-end">
                     <div className="flex w-96">
