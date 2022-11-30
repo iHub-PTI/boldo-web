e6e5dfc261206ce576ff4fc25d0647bc36e7ecfc
elias7991 <eliascaceres124@gmail.com>
2022-11-29 22:06:33 -0300

    feat: import svg as component

--- src/components/calendar/AppointmentCard.tsx
+++ src/components/calendar/AppointmentCard.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { ReactComponent as Close } from '../../assets/close-orange.svg';
 import { ReactComponent as TimeSlot } from '../../assets/time-slot.svg';
 import { ReactComponent as Check } from '../../assets/check-small.svg';
+import { ReactComponent as VirtualIcon } from '../../assets/virtual-appointment.svg';
+import { ReactComponent as PresentialIcon } from '../../assets/presential-appointment.svg';
+import { ReactComponent as CancelVirtualIcon } from '../../assets/cancel-virtual-appointment.svg';
+import { ReactComponent as CancelPresentialIcon } from '../../assets/cancel-presential-appointment.svg';
 
 
 function AppointmentCard(eventInfo) {
