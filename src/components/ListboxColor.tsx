5e8b4771af346850888c0b5a3251b23a4f838cfb
elias7991 <eliascaceres124@gmail.com>
2022-12-23 16:26:11 -0300

    fix: value change to id

--- src/components/ListboxColor.tsx
+++ src/components/ListboxColor.tsx
@@ -2,18 +2,18 @@ import React, { useState, useEffect } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
 
 type Props = {
-  data: { value: string; name: string; color: string }[]
+  data: { id: string; name: string; colorCode: string }[]
   label?: string
-  value?: string
+  id?: string
   onChange?: (value: string) => void
 }
 
-const ListboxComponent: React.FC<Props> = ({ data, label, value, onChange }) => {
-  const [selected, setSelected] = useState(value || data[0].value)
+const ListboxComponent: React.FC<Props> = ({ data, label, id, onChange }) => {
+  const [selected, setSelected] = useState(id || data[0].id)
 
   useEffect(() => {
-    if (value) setSelected(value)
-  }, [value])
+    if (id) setSelected(id)
+  }, [id])
 
   return (
     <Listbox
@@ -33,13 +33,13 @@ const ListboxComponent: React.FC<Props> = ({ data, label, value, onChange }) => 
           <div className='relative'>
             <span className='inline-block w-full rounded-md shadow-sm'>
               <Listbox.Button className='relative w-full py-2 pl-8 pr-10 text-left transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md cursor-default focus:outline-none'>
-                <span className='block truncate font-semibold' title={data.find(item => item.value === selected)?.name}>
-                  {data.find(item => item.value === selected)?.name}
+                <span className='block truncate font-semibold' title={data.find(item => item.id === selected)?.name}>
+                  {data.find(item => item.id === selected)?.name}
                 </span>
                 <div className={`absolute inset-y-0 left-0 flex items-center pl-1.5 ml-1`}>
                   <div
                     className='h-4 w-2'
-                    style={{ backgroundColor: data.find(item => item.value === selected)?.color }}
+                    style={{ backgroundColor: data.find(item => item.id === selected)?.colorCode }}
                   ></div>
                 </div>
                 <span className='absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none'>
@@ -67,7 +67,7 @@ const ListboxComponent: React.FC<Props> = ({ data, label, value, onChange }) => 
                 className='py-1 overflow-auto text-base leading-6 rounded-md shadow-xs max-h-60 focus:outline-none sm:text-sm sm:leading-5'
               >
                 {data.map(item => (
-                  <Listbox.Option key={item.value} value={item.value}>
+                  <Listbox.Option key={item.id} value={item.id}>
                     {({ selected, active }) => (
                       <div className={`cursor-default select-none relative py-2 pl-8 pr-4 hover:bg-gray-100`}>
                         <span
@@ -77,7 +77,7 @@ const ListboxComponent: React.FC<Props> = ({ data, label, value, onChange }) => 
                           {item.name}
                         </span>
                         <div className={`absolute inset-y-0 left-0 flex items-center pl-1.5 ml-1`}>
-                          <div className='h-4 w-2' style={{ backgroundColor: item.color }}></div>
+                          <div className='h-4 w-2' style={{ backgroundColor: item.colorCode }}></div>
                         </div>
                       </div>
                     )}
