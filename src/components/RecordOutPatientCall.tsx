ff76cd3cd1332cba00789a1a797599fdc467898e
sebasrival <sebasrival@gmail.com>
2023-01-26 10:30:38 -0300

    feat: added hanblde out side click container

--- src/components/RecordOutPatientCall.tsx
+++ src/components/RecordOutPatientCall.tsx
@@ -91,6 +91,8 @@ const RecordOutPatientCall: React.FC<Props> = ({ children, appointment }) => {
 
   const { addErrorToast } = useToasts()
 
+  const container = useRef<HTMLDivElement>(null)
+
   //methods
   const getRecordsPatient = async ({ doctorId = 'ALL', content = '', count = 10, offset = 1, order = 'DESC' }) => {
     setError(false)
@@ -229,175 +231,191 @@ const RecordOutPatientCall: React.FC<Props> = ({ children, appointment }) => {
     setActiveID('')
   }, [recordsPatient])
 
+  useEffect(() => {
+    function handleOutsideClick(event: MouseEvent) {
+      if (!container.current?.contains(event.target as Node)) {
+        if (!recordOutPatientButton) return
+        setRecordOutPatientButton(false)
+        setShowDetail(false)
+        setActiveID('')
+      }
+    }
+    window.addEventListener('click', handleOutsideClick, true)
+    return () => window.removeEventListener('click', handleOutsideClick, true)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [recordOutPatientButton])
+
   return (
     <div className='flex flex-no-wrap relative h-full'>
-      <div className={`flex flex-col absolute group ${hoverSidebar && 'cursor-pointer w-60'} w-24 inset-0 z-50 bg-gray-100 transition-all duration-500`}
-        onMouseOver={() => handleSidebarHoverOn()}
-        onMouseLeave={() => handleSidebarHoverOff()}
-      >
-        <div className='flex flex-col w-full flex-no-wrap mt-10 items-center'>
-          <img src={appointment.patient.photoUrl} alt='Foto de Perfil'
-            className='border-1 border-white rounded-full w-15 h-15 object-cover' />
-        </div>
-        <Disclosure>
-          {({ open }) => (
-            <div className={`w-0 ${hoverSidebar && 'w-auto opacity-100'} opacity-0 flex flex-col justify-center bg-white rounded-lg mx-2 p-2 gap-5 mt-5 mb-5 truncate`}>
-              <Disclosure.Button className="focus:outline-none ">
-                <div className='flex flex-row flex-no-wrap justify-center text-xl text-cool-gray-700 font-semibold truncate'>
-                  <span className='truncate'> {toUpperLowerCase(appointment.patient.givenName.split(' ')[0] + ' ' + appointment.patient.familyName.split(' ')[0])}</span>
-                  <ArrowDown className={`${open ? 'rotate-180 transform' : ''}`} />
-                </div>
-                <span className='font-semibold text-base' style={{ color: '#ABAFB6' }}>{appointment.patient.identifier == null || appointment.patient.identifier.includes('-')
-                  ? 'Paciente sin cédula'
-                  : 'CI ' + appointment.patient.identifier}</span>
-              </Disclosure.Button>
-              <Disclosure.Panel className="focus:outline-none ">
-                <div className='flex flex-col'>
-                  <span className='text-base font-normal' style={{ color: '#ABAFB6' }}>Edad</span>
-                  <span className='text-lg font-semibold text-cool-gray-700'>{differenceInYears(Date.now(), new Date(appointment.patient.birthDate))}</span>
-                </div>
-                <div className='flex flex-col'>
-                  <span className='text-base font-normal' style={{ color: '#ABAFB6' }}>Profesion</span>
-                  <span className='text-lg font-semibold text-cool-gray-700'>{appointment.patient.job}</span>
-                </div>
-                <div className='flex flex-col'>
-                  <span className='text-base font-normal' style={{ color: '#ABAFB6' }}>Teléfono</span>
-                  <span className='text-lg font-semibold text-cool-gray-700'>{appointment.patient.phone}</span>
-                </div>
-                <div className='flex flex-col'>
-                  <span className='text-base font-normal' style={{ color: '#ABAFB6' }}>Ciudad</span>
-                  <span className='text-lg font-semibold text-cool-gray-700'>{appointment.patient.city}</span>
-                </div>
-              </Disclosure.Panel>
-            </div>
-          )}
-        </Disclosure>
-        <div className='flex flex-row flex-no-wrap justify-center items-center '>
-          <button className={`flex flex-row flex-no-wrap justify-center items-center p-2 focus:outline-none`} onClick={() => onClickOutPatientRecord()}>
-            <UserCircle className='w-5 h-5' fill={`${recordOutPatientButton ? '#13A5A9' : '#6B7280'}`} />
-            <div className={`ml-1 w-0 ${hoverSidebar && 'w-11/12 opacity-100'} opacity-0 flex text-base font-medium text-gray-500 truncate ${recordOutPatientButton && 'text-primary-600 font-semibold'}`} style={{ transition: 'width 0.5s linear, opacity 0.5s linear' }}>Registro Ambulatorio</div>
-          </button>
-        </div>
-      </div>
-      <div
-        className={`opacity-0 ${recordOutPatientButton && 'opacity-100'} flex flex-col z-50 absolute left-60 h-full transition-all duration-300 rounded-r-xl`}
-        style={{ width: recordOutPatientButton ? '21.6rem' : '0px' }}
-      >
-        <div className='flex flex-row flex-no-wrap bg-primary-500 h-10 rounded-tr-xl pl-3 py-2 pr-2 items-center justify-between'>
-          <span className='text-white font-medium text-sm truncate'>Registro de consultas ambulatorias</span>
-          <button className='focus:outline-none' onClick={() => {
-            setRecordOutPatientButton(false)
-            setShowDetail(false)
-            setActiveID('')
-          }}>
-            <CloseButton />
-          </button>
-        </div>
-        <div className={`flex flex-col flex-1 p-2 items-center`} style={stylePanelSidebar}>
-          <div className='flex flex-row flew-no-wrap w-full items-center'>
-            {!showDetail &&
-              <div className='w-full h-11 relative bg-cool-gray-50 rounded-lg mb-5 mt-5 mr-2'>
-                <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
-                  <SearchIcon className='w-5 h-5' />
-                </div>
-                <input
-                  className='p-3 pl-10 w-full outline-none hover:bg-cool-gray-100 transition-colors delay-200 rounded-lg'
-                  type='search'
-                  name='search'
-                  placeholder='Motivo principal de la visita'
-                  title='Escriba el motivo principal de la visita'
-                  autoComplete='off'
-                  value={inputContent}
-                  onChange={e => {
-                    setInputContent(e.target.value)
-                    debounce(e.target.value.trim())
-                  }}
-                />
-              </div>}
-            {!showDetail &&
-              <QueryFilter
-                currentDoctor={doctor}
-                setFilterAuthor={setFilterDoctor}
-                setOrder={setFilterOrder}
-                getApiCall={getRecordsPatient}
-                inputContent={inputContent}
-                countPage={countPage}
-                activeColor={true}
-              />}
+      <div className='p-0 m-0 flex flex-no-wrap h-full' ref={container}>
+        <div className={`flex flex-col absolute group ${hoverSidebar && 'cursor-pointer w-60'} w-24 inset-0 z-50 bg-gray-100 transition-all duration-500`}
+          onMouseOver={() => handleSidebarHoverOn()}
+          onMouseLeave={() => handleSidebarHoverOff()}
+        >
+          <div className='flex flex-col w-full flex-no-wrap mt-10 items-center'>
+            <img src={appointment.patient.photoUrl} alt='Foto de Perfil'
+              className='border-1 border-white rounded-full w-15 h-15 object-cover' />
           </div>
-          {!showDetail && !error &&
-            <div
-              className={`flex flex-col overflow-x-hidden mx-1 scrollbar w-full ${loading && 'justify-center items-center'
-                }`}
-              style={{ height: 'calc(100vh - 230px)' }}
-              ref={scrollEvent}
-              onScroll={() => onScrollEnd()}
-            >
-              {loading && <SpinnerLoading />}
-              {!loading &&
-                recordsPatient.map((record, index) => (
-                  <PatientRecord
-                    key={index}
-                    patientRecord={record}
-                    getRecordPatientDetail={getRecordPatientDetail}
-                    selected={activeID === record.encounterDto.id}
-                    onActiveID={setActiveID}
-                    darkMode={true}
-                    onShowDetail={() => { setShowDetail(true) }}
-                    isCall={true}
+          <Disclosure>
+            {({ open }) => (
+              <div className={`w-0 ${hoverSidebar && 'w-auto opacity-100'} opacity-0 flex flex-col justify-center bg-white rounded-lg mx-2 p-2 gap-5 mt-5 mb-5 truncate`}>
+                <Disclosure.Button className="focus:outline-none ">
+                  <div className='flex flex-row flex-no-wrap justify-center text-xl text-cool-gray-700 font-semibold truncate'>
+                    <span className='truncate'> {toUpperLowerCase(appointment.patient.givenName.split(' ')[0] + ' ' + appointment.patient.familyName.split(' ')[0])}</span>
+                    <ArrowDown className={`${open ? 'rotate-180 transform' : ''}`} />
+                  </div>
+                  <span className='font-semibold text-base' style={{ color: '#ABAFB6' }}>{appointment.patient.identifier == null || appointment.patient.identifier.includes('-')
+                    ? 'Paciente sin cédula'
+                    : 'CI ' + appointment.patient.identifier}</span>
+                </Disclosure.Button>
+                <Disclosure.Panel className="focus:outline-none ">
+                  <div className='flex flex-col'>
+                    <span className='text-base font-normal' style={{ color: '#ABAFB6' }}>Edad</span>
+                    <span className='text-lg font-semibold text-cool-gray-700'>{differenceInYears(Date.now(), new Date(appointment.patient.birthDate))}</span>
+                  </div>
+                  <div className='flex flex-col'>
+                    <span className='text-base font-normal' style={{ color: '#ABAFB6' }}>Profesion</span>
+                    <span className='text-lg font-semibold text-cool-gray-700'>{appointment.patient.job}</span>
+                  </div>
+                  <div className='flex flex-col'>
+                    <span className='text-base font-normal' style={{ color: '#ABAFB6' }}>Teléfono</span>
+                    <span className='text-lg font-semibold text-cool-gray-700'>{appointment.patient.phone}</span>
+                  </div>
+                  <div className='flex flex-col'>
+                    <span className='text-base font-normal' style={{ color: '#ABAFB6' }}>Ciudad</span>
+                    <span className='text-lg font-semibold text-cool-gray-700'>{appointment.patient.city}</span>
+                  </div>
+                </Disclosure.Panel>
+              </div>
+            )}
+          </Disclosure>
+          <div className='flex flex-row flex-no-wrap justify-center items-center '>
+            <button className={`flex flex-row flex-no-wrap justify-center items-center p-2 focus:outline-none`} onClick={() => onClickOutPatientRecord()}>
+              <UserCircle className='w-5 h-5' fill={`${recordOutPatientButton ? '#13A5A9' : '#6B7280'}`} />
+              <div className={`ml-1 w-0 ${hoverSidebar && 'w-11/12 opacity-100'} opacity-0 flex text-base font-medium text-gray-500 truncate ${recordOutPatientButton && 'text-primary-600 font-semibold'}`} style={{ transition: 'width 0.5s linear, opacity 0.5s linear' }}>Registro Ambulatorio</div>
+            </button>
+          </div>
+        </div>
+        <div
+          className={`opacity-0 ${recordOutPatientButton && 'opacity-100'} flex flex-col z-50 absolute left-60 h-full transition-all duration-300 rounded-r-xl`}
+          style={{ width: recordOutPatientButton ? '21.6rem' : '0px' }}
+        >
+          <div className='flex flex-row flex-no-wrap bg-primary-500 h-10 rounded-tr-xl pl-3 py-2 pr-2 items-center justify-between'>
+            <span className='text-white font-medium text-sm truncate'>Registro de consultas ambulatorias</span>
+            <button className='focus:outline-none' onClick={() => {
+              setRecordOutPatientButton(false)
+              setShowDetail(false)
+              setActiveID('')
+            }}>
+              <CloseButton />
+            </button>
+          </div>
+          <div className={`flex flex-col flex-1 p-2 items-center`} style={stylePanelSidebar}>
+            <div className='flex flex-row flew-no-wrap w-full items-center'>
+              {!showDetail &&
+                <div className='w-full h-11 relative bg-cool-gray-50 rounded-lg mb-5 mt-5 mr-2'>
+                  <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
+                    <SearchIcon className='w-5 h-5' />
+                  </div>
+                  <input
+                    className='p-3 pl-10 w-full outline-none hover:bg-cool-gray-100 transition-colors delay-200 rounded-lg'
+                    type='search'
+                    name='search'
+                    placeholder='Motivo principal de la visita'
+                    title='Escriba el motivo principal de la visita'
+                    autoComplete='off'
+                    value={inputContent}
+                    onChange={e => {
+                      setInputContent(e.target.value)
+                      debounce(e.target.value.trim())
+                    }}
                   />
-                ))}
-
-              {!loading && totalRecordsPatient === 0 &&
-                <div className='flex flex-row w-full justify-center items-center text-white font-bold'>
-                  No se encontraron resultados.
-                </div>
-              }
-            </div>}
-
-          {error && <div className='flex flex-col w-full h-full items-center justify-center'>
-            <div className='mt-6 text-center sm:mt-5'>
-              <h3 className='text-lg font-medium leading-6 text-white' id='modal-headline'>
-                Ha ocurrido un error al traer los registros
-              </h3>
+                </div>}
+              {!showDetail &&
+                <QueryFilter
+                  currentDoctor={doctor}
+                  setFilterAuthor={setFilterDoctor}
+                  setOrder={setFilterOrder}
+                  getApiCall={getRecordsPatient}
+                  inputContent={inputContent}
+                  countPage={countPage}
+                  activeColor={true}
+                />}
             </div>
-            <div className='flex justify-center w-full mt-6 sm:mt-8'>
-              <button
-                className='px-4 py-2 text-lg font-medium text-white border border-transparent rounded-md shadow-sm bg-primary-500 hover:bg-primary-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500 sm:col-start-2'
-                onClick={() => getRecordsPatient({ offset: 1, count: countPage })}
+            {!showDetail && !error &&
+              <div
+                className={`flex flex-col overflow-x-hidden mx-1 scrollbar w-full ${loading && 'justify-center items-center'
+                  }`}
+                style={{ height: 'calc(100vh - 230px)' }}
+                ref={scrollEvent}
+                onScroll={() => onScrollEnd()}
               >
-                Inténtar de nuevo
-              </button>
-            </div>
-          </div>}
+                {loading && <SpinnerLoading />}
+                {!loading &&
+                  recordsPatient.map((record, index) => (
+                    <PatientRecord
+                      key={index}
+                      patientRecord={record}
+                      getRecordPatientDetail={getRecordPatientDetail}
+                      selected={activeID === record.encounterDto.id}
+                      onActiveID={setActiveID}
+                      darkMode={true}
+                      onShowDetail={() => { setShowDetail(true) }}
+                      isCall={true}
+                    />
+                  ))}
 
+                {!loading && totalRecordsPatient === 0 &&
+                  <div className='flex flex-row w-full justify-center items-center text-white font-bold'>
+                    No se encontraron resultados.
+                  </div>
+                }
+              </div>}
 
-          {!showDetail && !error &&
-            <div className='flex flex-row flex-no-wrap relative w-full justify-center'>
-              <img className={`${!loadingScroll && 'hidden'} absolute w-15 h-15 z-50`} src={loadGif} alt='loading gif' style={{ bottom: '-2rem' }} />
-            </div>
-          }
-
-          {showDetail && !error &&
-            <div className='flex flex-col flex-no-wrap w-full flex-1'>
-              <div className="flex flex-row flex-no-wrap items-start">
-                <button className='flex flex-row text-white focus:outline-none justify-center items-center gap-1' onClick={() => setShowDetail(false)}>
-                  <ArrowBackIOS fill='#FFFFFF' style={{
-                    margin: '0 auto',
-                    transform: 'scale(.7)',
-                  }} /> atrás
+            {error && <div className='flex flex-col w-full h-full items-center justify-center'>
+              <div className='mt-6 text-center sm:mt-5'>
+                <h3 className='text-lg font-medium leading-6 text-white' id='modal-headline'>
+                  Ha ocurrido un error al traer los registros
+                </h3>
+              </div>
+              <div className='flex justify-center w-full mt-6 sm:mt-8'>
+                <button
+                  className='px-4 py-2 text-lg font-medium text-white border border-transparent rounded-md shadow-sm bg-primary-500 hover:bg-primary-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500 sm:col-start-2'
+                  onClick={() => getRecordsPatient({ offset: 1, count: countPage })}
+                >
+                  Inténtar de nuevo
                 </button>
               </div>
-              <div
-                className={`flex flex-col w-full overflow-y-auto scrollbar items-center justify-center mt-5`}
-                style={{ height: 'calc(100vh - 230px)' }}
-              >
+            </div>}
 
-                {loadingDetail && <SpinnerLoading />}
-                {!loadingDetail && <DescripcionRecordPatientDetail data={detailRecordPatient} darkMode={true} isCall={true} />}
+
+            {!showDetail && !error &&
+              <div className='flex flex-row flex-no-wrap relative w-full justify-center'>
+                <img className={`${!loadingScroll && 'hidden'} absolute w-15 h-15 z-50`} src={loadGif} alt='loading gif' style={{ bottom: '-2rem' }} />
               </div>
-            </div>
-          }
+            }
+
+            {showDetail && !error &&
+              <div className='flex flex-col flex-no-wrap w-full flex-1'>
+                <div className="flex flex-row flex-no-wrap items-start">
+                  <button className='flex flex-row text-white focus:outline-none justify-center items-center gap-1' onClick={() => setShowDetail(false)}>
+                    <ArrowBackIOS fill='#FFFFFF' style={{
+                      margin: '0 auto',
+                      transform: 'scale(.7)',
+                    }} /> atrás
+                  </button>
+                </div>
+                <div
+                  className={`flex flex-col w-full overflow-y-auto scrollbar items-center justify-center mt-5`}
+                  style={{ height: 'calc(100vh - 230px)' }}
+                >
+
+                  {loadingDetail && <SpinnerLoading />}
+                  {!loadingDetail && <DescripcionRecordPatientDetail data={detailRecordPatient} darkMode={true} isCall={true} />}
+                </div>
+              </div>
+            }
+          </div>
         </div>
       </div>
       {children}
