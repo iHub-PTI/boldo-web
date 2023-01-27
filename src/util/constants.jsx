a0deaef9caeb57ac7ccdaf897f41d8be38ecd07b
sebasrival <sebasrival@gmail.com>
2023-01-27 18:09:29 -0300

    fix: vertical scroll -> PatientSection.tsx

--- src/util/constants.jsx
+++ src/util/constants.jsx
@@ -2,3 +2,8 @@
 
 
 export const TIME_TO_OPEN_APPOINTMENT = '15'
+
+//height of the layout container in px
+export const HEIGHT_NAVBAR = 63
+export const HEIGHT_BAR_STATE_APPOINTMENT = 52
+export const WIDTH_XL = 1536
