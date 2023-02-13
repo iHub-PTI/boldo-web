a95c15e4674aee2267429d138e581c4441a62abe
elias7991 <eliascaceres124@gmail.com>
2023-02-13 14:36:56 -0300

    style: flex-col change to flex-row

--- src/components/studiesorder/Cards/DiagnosticReportCard.tsx
+++ src/components/studiesorder/Cards/DiagnosticReportCard.tsx
@@ -39,10 +39,10 @@ const DiagnosticReportCard = ({
   return (
     // all the card
     <div
-      className={`flex flex-row pl-1 pt-1 ${isCall ? 'w-full' : 'w-64'} h-28 rounded-lg group cursor-pointer mb-1 mx-1 truncate pr-1`}
+      className={`flex flex-col pl-1 pt-1 ${isCall ? 'w-full' : 'w-64'} rounded-lg group cursor-pointer mb-1 mx-1 truncate pr-1`}
       style={{
         backgroundColor: background,
-        minHeight: '102px'
+        minHeight: '120px'
       }}
       onClick={() => {
         getReportDetail(report.id)
@@ -53,22 +53,22 @@ const DiagnosticReportCard = ({
       onMouseLeave={() => { updateBackground() }}
       {...props}
     >
-      {/* first column - icon for type of category */}
-      <div className='flex flex-col w-2/12 m-1'>
-        { report &&
-          (report.category.toLowerCase() === categoryIssuedOrders.LABORATORY.toLowerCase()
-            ? <IconLab height={24} width={24}/>
-            : report.category.toLowerCase() === categoryIssuedOrders.IMAGE.toLowerCase()
-              ? <IconImg height={24} width={24}/>
-              : <IconOther height={24} width={24}/>
-          )
-        }
-      </div>
-      {/* second column */}
-      <div className='flex flex-col w-full break-all'>
+      {/* icon and title */}
+      <div className='flex flex-row '>
+        {/* icon for type of category */}
+        <div>
+          { report &&
+            (report.category.toLowerCase() === categoryIssuedOrders.LABORATORY.toLowerCase()
+              ? <IconLab height={24} width={24}/>
+              : report.category.toLowerCase() === categoryIssuedOrders.IMAGE.toLowerCase()
+                ? <IconImg height={24} width={24}/>
+                : <IconOther height={24} width={24}/>
+            )
+          }
+        </div>
         {/* title for type of category */}
         <div
-          className={`font-semibold text-base truncate w-full ${darkMode ? 'group-hover:text-gray-700 text-white' : 'text-gray-700'}`}
+          className={`font-semibold ml-2 text-base truncate w-full ${darkMode ? 'group-hover:text-gray-700 text-white' : 'text-gray-700'}`}
         >
           {report &&
             (report.category.toLowerCase() === categoryIssuedOrders.LABORATORY.toLowerCase()
@@ -79,40 +79,40 @@ const DiagnosticReportCard = ({
             )
           }
         </div>
-        {/* description of the study */}
-        <div
-          className={`text-base mr-1 pb-1 truncate ${darkMode ? 'group-hover:text-gray-700 text-white' : 'text-gray-700'}`}
-          title={report.description ?? 'No se ha agregado la descripción'}
-        >
-          {report.description ?? 'Sin descripción'}
-        </div>
-        {/* source of the study - row of information (patient or anyone) */}
-        <div className='flex flex-row w-full'>
-          {/* highlighted text */}
-          <span className={`font-normal group-hover:text-primary-500 ${darkMode ? 'group-hover:text-gray-500 text-white' : 'text-gray-500'} mr-1`}>Subido por:</span>
-          {/* source */}
-          <div 
-            className={`${darkMode ? 'group-hover:text-gray-700 text-white' : 'text-gray-700'}`}
-            title={report.source ?? 'Origen desconocido'}
-          >
-              {report.source ?? 'Origen desconocido'}
-          </div>
-        </div>
-        {/* row that contains date and number of the days elapsed */}
-        <div className='flex flex-row gap-2'>
-          {/* date */}
-          <div className={`font-normal ${darkMode ? 'group-hover:text-gray-700 text-white' : 'text-gray-700'}`}>
-            {moment(report.effectiveDate).format('DD/MM/YYYY')}
-          </div>
-          {/* number of the days */}
-          <div 
-            className={`${darkMode ? 'group-hover:text-gray-500 text-white' : 'text-gray-500'}`}
-          >
-            {countDays(report.effectiveDate.toString())}
-          </div>
-        </div>
-        <span className='w-full h-0' style={{ borderBottom: darkMode ? '1px solid rgba(247, 244, 244, 0.2)' : '1px solid #F7F4F4' }}></span>
       </div>
+      {/* description of the study */}
+      <div
+        className={`text-base mr-1 pb-1 truncate ${darkMode ? 'group-hover:text-gray-700 text-white' : 'text-gray-700'}`}
+        title={report.description ?? 'No se ha agregado la descripción'}
+      >
+        {report.description ?? 'Sin descripción'}
+      </div>
+      {/* source of the study - row of information (patient or anyone) */}
+      <div className='flex flex-row w-full'>
+        {/* highlighted text */}
+        <span className={`font-normal group-hover:text-primary-500 ${darkMode ? 'group-hover:text-gray-500 text-white' : 'text-gray-500'} mr-1`}>Subido por:</span>
+        {/* source */}
+        <div 
+          className={`${darkMode ? 'group-hover:text-gray-700 text-white' : 'text-gray-700'}`}
+          title={report.source ?? 'Origen desconocido'}
+        >
+            {report.source ?? 'Origen desconocido'}
+        </div>
+      </div>
+      {/* row that contains date and number of the days elapsed */}
+      <div className='flex flex-row gap-2'>
+        {/* date */}
+        <div className={`font-normal ${darkMode ? 'group-hover:text-gray-700 text-white' : 'text-gray-700'}`}>
+          {moment(report.effectiveDate).format('DD/MM/YYYY')}
+        </div>
+        {/* number of the days */}
+        <div 
+          className={`${darkMode ? 'group-hover:text-gray-500 text-white' : 'text-gray-500'}`}
+        >
+          {countDays(report.effectiveDate.toString())}
+        </div>
+      </div>
+      <span className='w-full h-0' style={{ borderBottom: darkMode ? '1px solid rgba(247, 244, 244, 0.2)' : '1px solid #F7F4F4' }}></span>
     </div>
   );
 }
