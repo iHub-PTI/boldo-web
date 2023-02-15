86bcd09617920d567ad3906390ec4d05ab8dd4ea
elias7991 <eliascaceres124@gmail.com>
2023-02-15 18:36:57 -0300

    fix: hide cancel appointment button when doesn't upcoming

--- src/pages/inperson/MedicalRecordSection.jsx
+++ src/pages/inperson/MedicalRecordSection.jsx
@@ -746,36 +746,39 @@ export default ({ appointment, setDisabledRedcordButton }) => {
         >
           <CancelAppointmentModal isOpen={isOpen} setIsOpen={setIsOpen} appointmentId={id} />
         </div>
-        <div className='flex flex-row-reverse mt-6'>
-          <div className='ml-6'>
-            <Button
-              disabled={disableBCancel}
-              className={classes.muiButtonOutlined}
-              variant='outlined'
-              onClick={() => {
-                setIsOpen(true)
-              }}
-              endIcon={
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  className='h-6 w-6'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  stroke='currentColor'
+        { appointment?.status === 'upcoming'
+          ? <div className='flex flex-row-reverse mt-6'>
+              <div className='ml-6'>
+                <Button
+                  disabled={disableBCancel}
+                  className={classes.muiButtonOutlined}
+                  variant='outlined'
+                  onClick={() => {
+                    setIsOpen(true)
+                  }}
+                  endIcon={
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      className='h-6 w-6'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      stroke='currentColor'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth='2'
+                        d='M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16'
+                      />
+                    </svg>
+                  }
                 >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeWidth='2'
-                    d='M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16'
-                  />
-                </svg>
-              }
-            >
-              Cancelar cita
-            </Button>
-          </div>
-        </div>
+                  Cancelar cita
+                </Button>
+              </div>
+            </div>
+          : <div></div>
+        }
         <Typography style={{ marginTop: '10px' }} variant='body2' color='textSecondary'>
           Una vez culminada la cita, dispondrá de 2 horas para actualizar las notas médicas del paciente.
         </Typography>
