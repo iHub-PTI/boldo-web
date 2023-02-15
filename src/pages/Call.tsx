b03f02392b9b13bb12c15b0561304556f45b7f29
elias7991 <eliascaceres124@gmail.com>
2023-02-15 17:28:09 -0300

    fix: there is not prescriptions in call

--- src/pages/Call.tsx
+++ src/pages/Call.tsx
@@ -356,7 +356,11 @@ const Gate = () => {
                 }
               } else {
                 console.log("there is not prescriptions");
-                addToast({ type: 'info', title: 'Atención!', text: 'Debe agregar alguna receta para imprimirla.' });
+                if (appointment?.status !== 'locked') {
+                  addToast({ type: 'info', title: 'Atención!', text: 'Debe agregar alguna receta para imprimirla.' });
+                } else if (appointment?.status === 'locked') {
+                  addToast({ type:'info', title: 'Atención!', text: 'No posee recetas para imprimir.' })
+                }
               }
             }}
           />
