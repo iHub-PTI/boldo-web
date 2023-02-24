ba4e600198f7132959ffa169336f01de8feb8fe0
elias7991 <eliascaceres124@gmail.com>
2023-02-24 20:40:26 -0300

    feat: component for info tooltip

---
+++ src/components/hovers/TooltipInfo.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import InfoIcon from '../icons/info-icons/InfoIcon';
+
+
+type Props = {
+  backgorundColor?: string;
+}
+
+
+const TooltipInfo = (props: Props) => {
+  const {backgorundColor="#364152"} = props
+
+  return(
+    // all the tooltip
+    <div 
+      className={`flex flex-row w-80 h-auto z-50 p-4 gap-3 absolute rounded-tl-lg rounded-br-lg rounded-bl-lg transform`}
+      style={{ background: backgorundColor, translate: "-20rem", marginTop: "1.5rem" }}
+    >
+      {/* this is the icon on left */}
+      <div className='h-full w-1/12 justify-center'>
+        <InfoIcon circularColor='#FFFFFF'/>
+      </div>
+      {/* this is the space for text info */}
+      <div className='flex flex-col h-full w-11/12 text-white'>
+        <p>Podés editar el dignósitco para dar mejor contexto.</p>
+        <p>Esto no altera el diagnóstico consignado en el registro ambulatorio.</p>
+      </div>
+    </div>
+  )
+}
+
+
+export default TooltipInfo
