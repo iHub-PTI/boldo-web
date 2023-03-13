d24aa36d7b1e6a122fe0bf9c81bfcdaf943e483b
elias7991 <eliascaceres124@gmail.com>
2023-03-13 19:39:05 -0300

    feat: organization bar in virtual appointment

--- src/util/constants.jsx
+++ src/util/constants.jsx
@@ -4,7 +4,7 @@
 export const TIME_TO_OPEN_APPOINTMENT = '15'
 
 //height of the layout container in px
-export const HEIGHT_NAVBAR = 63
+export const HEIGHT_NAVBAR = 64
 export const ORGANIZATION_BAR = 24
 export const HEIGHT_BAR_STATE_APPOINTMENT = 52
 export const WIDTH_XL = 1536
