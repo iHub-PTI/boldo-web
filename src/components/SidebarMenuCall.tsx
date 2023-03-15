1648ce5889ba336b90fdc0451bd604227357b477
sebasrival <sebasrival@gmail.com>
2023-03-15 18:21:25 -0300

    feat: Added Medical History

--- src/components/SidebarMenuCall.tsx
+++ src/components/SidebarMenuCall.tsx
@@ -16,6 +16,11 @@ import { useToasts } from './Toast';
 import CloseButton from './icons/CloseButton';
 import NoProfilePicture from './icons/NoProfilePicture';
 import HistoryIcon from './icons/HistoryIcon';
+import CardListWarning from './medical-history/CardListWarning';
+import CardList from './medical-history/CardList';
+import TableGynecology from './medical-history/TableGynecology';
+import PersonOutline from './icons/PersonOutline';
+import FamilyIcon from './icons/FamilyIcon';
 
 const stylePanelSidebar = {
   background: 'rgba(107, 107, 107, 0.56)',
@@ -766,13 +771,161 @@ const RecordOutPatient: React.FC<RecordOutPatientType> = ({
   )
 }
 
+
+type MedicalHistoryCallType = {
+  containerRef: MutableRefObject<HTMLDivElement>,
+  appointment: AppointmentWithPatient;
+  medicalHistoryButton: boolean,
+  setMedicalHistoryButton: (value: boolean) => void
+  handleSidebarHoverOff: () => void,
+}
+
+/**
+ * Medical History Component Call
+ */
+const MedicalHistoryCall: React.FC<MedicalHistoryCallType> = ({
+  containerRef,
+  appointment: AppointmentWithPatient,
+  medicalHistoryButton,
+  setMedicalHistoryButton,
+  handleSidebarHoverOff
+}) => {
+
+  useEffect(() => {
+    function handleOutsideClick(event: MouseEvent) {
+      if (!containerRef.current?.contains(event.target as Node)) {
+        if (!medicalHistoryButton) return
+        setMedicalHistoryButton(false)
+      }
+    }
+    window.addEventListener('click', handleOutsideClick, true)
+    return () => window.removeEventListener('click', handleOutsideClick, true)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [medicalHistoryButton])
+
+  useEffect(() => {
+    if (medicalHistoryButton) return
+    handleSidebarHoverOff()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [medicalHistoryButton])
+
+  return (
+    <div
+      className={`opacity-0 ${medicalHistoryButton && 'opacity-100'} flex flex-col z-50 absolute left-60 h-full transition-all duration-300 rounded-r-xl`}
+      style={{ width: medicalHistoryButton ? '21.6rem' : '0px' }}
+    >
+      <div className='flex flex-row flex-no-wrap bg-primary-500 h-10 rounded-tr-xl pl-3 py-2 pr-2 items-center justify-between'>
+        <span className='text-white font-medium text-sm truncate'>Antecedentes Clínicos</span>
+        <button className='focus:outline-none' onClick={() => {
+          setMedicalHistoryButton(false)
+
+        }}>
+          <CloseButton />
+        </button>
+      </div>
+      <div className={`flex flex-col flex-1 py-2 pr-2 items-center gap-4 scrollbar overflow-y-auto`} style={stylePanelSidebar}>
+        {/* Personal */}
+        <div className='flex flex-row flew-no-wrap w-full'>
+          <div className='flex flew-row flex-no-wrap w-full border-l-4 h-10 px-6 items-center rounded-r-md' style={{ backgroundColor: '#f7f4f4', borderColor: '#FFFFFF' }}>
+            <span className='flex flex-row font-medium text-cool-gray-700 tracking-wide gap-2'>
+              <PersonOutline fill={"#364152"} />
+              Personal
+            </span>
+          </div>
+        </div>
+        <div className='flex flex-col w-full pl-6 pr-2'>
+          <CardListWarning
+            title='Alergias y sensibilidades'
+            dataList={[]}
+            url={''}
+            patientId={''}
+            handlerSaveLoading={(value: boolean) => { }}
+          />
+        </div>
+        {/* Section pathology */}
+        <div className='flex flex-col w-full pl-8 pr-6'>
+          <div className='font-medium text-base w-full' style={{color: "#DB7D68"}}>Patologías</div>
+          <div className='flex flex-col w-full pl-2 pr-1 gap-1'>
+            <CardList
+              title={'Cardiopatías'}
+              dataList={[]}
+              typeCode='cardiopathies'
+            />
+            <CardList
+              title={'Respiratorias'}
+              dataList={[]}
+              typeCode='respiratory'
+            />
+            <CardList
+              title={'Digestivas'}
+              dataList={[]}
+              typeCode='digestive'
+            />
+          </div>
+        </div>
+        <div className='flex flex-col w-full gap-7 mb-5 pl-3 pr-3'>
+          {/* Section procedures */}
+          <CardList
+            TitleElement={() => <div className='font-medium text-base text-primary-500'>Procedimientos</div>}
+            dataList={[]}
+            inputTypeWith="date"
+            typeCode='procedures'
+          />
+          {/* Section Others */}
+          <CardList
+            TitleElement={() => <div className='font-medium text-base text-primary-500'>Otros</div>}
+            dataList={[]}
+            inputTypeWith="date"
+            typeCode='others_personal'
+          />
+          {/* Section Gynecology */}
+          <TableGynecology
+            gynecology={[]}
+            typeCode='gynecology'
+          />
+        </div>
+        {/* Familiar */}
+        <div className='flex flex-col items-center w-full gap-3 pb-5'>
+          <div className='flex flex-row flew-no-wrap w-full'>
+            <div className='flex flew-row flex-no-wrap w-full border-l-4 h-10 px-6 items-center rounded-r-md' style={{ backgroundColor: '#f7f4f4', borderColor: '#FFFFFF' }}>
+              <span className='flex flex-row gap-2 font-medium text-cool-gray-700 tracking-wide'>
+                <FamilyIcon fill={"#364152"} />
+                Familiar
+              </span>
+            </div>
+          </div>
+          <div className='flex flex-col w-full pl-2 pr-1 gap-1 mt-5'>
+            <CardList
+              TitleElement={() => <div className='font-medium text-base text-primary-500'>Enfermedades hereditarias</div>}
+              dataList={[]}
+              inputTypeWith='relationship'
+              typeCode='hereditary_diseases'
+            />
+            <CardList
+              TitleElement={() =>
+                <div className='font-medium text-base text-primary-500'>Otros</div>
+              }
+              dataList={[]}
+              inputTypeWith="relationship"
+              typeCode='others_family'
+            />
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
 type PropsSidebarMenuCall = {
   children: React.ReactNode;
   appointment: AppointmentWithPatient;
 }
+
 const SidebarMenuCall: React.FC<PropsSidebarMenuCall> = ({ children, appointment }) => {
 
   const [recordOutPatientButton, setRecordOutPatientButton] = useState(false)
+  const [medicalHistoryButton, setMedicalHistoryButton] = useState(false)
 
   const [hoverSidebar, setHoverSidebar] = useState(false)
 
@@ -782,11 +935,18 @@ const SidebarMenuCall: React.FC<PropsSidebarMenuCall> = ({ children, appointment
 
   const handleSidebarHoverOff = () => {
     if (recordOutPatientButton) return
+    if (medicalHistoryButton) return
     setHoverSidebar(false)
   }
 
   const onClickOutPatientRecord = () => {
+    setMedicalHistoryButton(false)
     setRecordOutPatientButton(!recordOutPatientButton)
+  }
+
+  const medicalHistoryToggleButton = () => {
+    setRecordOutPatientButton(false)
+    setMedicalHistoryButton(!medicalHistoryButton)
   }
 
   const container = useRef<HTMLDivElement>(null)
@@ -854,9 +1014,9 @@ const SidebarMenuCall: React.FC<PropsSidebarMenuCall> = ({ children, appointment
               <UserCircle className='w-5 h-5' fill={`${recordOutPatientButton ? '#13A5A9' : '#6B7280'}`} />
               <div className={`ml-1 w-0 ${hoverSidebar && 'w-11/12 opacity-100'} opacity-0 flex text-base font-medium text-gray-500 truncate ${recordOutPatientButton && 'text-primary-600 font-semibold'}`} style={{ transition: 'width 0.5s linear, opacity 0.5s linear' }}>Registro Ambulatorio</div>
             </button>
-            <button className={`flex flex-row flex-no-wrap justify-center items-center p-2 focus:outline-none`} onClick={() => onClickOutPatientRecord()}>
-              <HistoryIcon fill={`${recordOutPatientButton ? '#13A5A9' : '#6B7280'}`} />
-              <div className={`ml-1 w-0 ${hoverSidebar && 'w-11/12 opacity-100'} opacity-0 flex text-base font-medium text-gray-500 truncate ${recordOutPatientButton && 'text-primary-600 font-semibold'}`} style={{ transition: 'width 0.5s linear, opacity 0.5s linear' }}>Antecedentes Clínicos</div>
+            <button className={`flex flex-row flex-no-wrap justify-center items-center p-2 focus:outline-none`} onClick={() => medicalHistoryToggleButton()}>
+              <HistoryIcon fill={`${medicalHistoryButton ? '#13A5A9' : '#6B7280'}`} />
+              <div className={`ml-1 w-0 ${hoverSidebar && 'w-11/12 opacity-100'} opacity-0 flex text-base font-medium text-gray-500 truncate ${medicalHistoryButton && 'text-primary-600 font-semibold'}`} style={{ transition: 'width 0.5s linear, opacity 0.5s linear' }}>Antecedentes Clínicos</div>
             </button>
           </div>
         </div>
@@ -867,12 +1027,19 @@ const SidebarMenuCall: React.FC<PropsSidebarMenuCall> = ({ children, appointment
           setRecordOutPatientButton={setRecordOutPatientButton}
           handleSidebarHoverOff={handleSidebarHoverOff}
         />
+
+        <MedicalHistoryCall
+          containerRef={container}
+          appointment={appointment}
+          medicalHistoryButton={medicalHistoryButton}
+          setMedicalHistoryButton={setMedicalHistoryButton}
+          handleSidebarHoverOff={handleSidebarHoverOff}
+        />
+
       </div>
       {children}
     </div>
   )
 }
-
-
 
 export default SidebarMenuCall
