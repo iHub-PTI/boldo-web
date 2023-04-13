45b7b20cc947c61af9f7cd0f7ed24be461cd602f
elias7991 <eliascaceres124@gmail.com>
2023-04-13 13:49:11 -0400

    feat: base object with error headers for sentry

---
+++ src/util/Sentry/errorHeaders.ts
@@ -0,0 +1,20 @@
+export const ERROR_HEADERS = {
+  GENERIC: "Generic error.",
+  UNKNOWN: "Unknown error.",
+  ORGANIZATION: {
+    FAILURE_GET: "Could not get organizations."
+  },
+  DOCTOR: {
+    FAILURE_GET_PROFILE: "could not get the profile of the doctor."
+  },
+  WAITING_ROOM: {
+    FAILURE_GET_APPOINTMENT: "Could not get appointments for waiting room."
+  },
+  PRIVATE_EVENTS: {
+    FAILURE_CREATE: "Could not create the private event.",
+    FAILURE_DELETE: "Could not delete the private event."
+  },
+  APPOINTMENT: {
+    FAILURE_GET_ON_DASHBOARD: "Could not reload the appointments on the Dashboard."
+  }
+}
