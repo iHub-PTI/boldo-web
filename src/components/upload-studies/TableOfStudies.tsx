22e4e192416619062cf13a9ee11cbb40606920ed
elias7991 <eliascaceres124@gmail.com>
2023-05-09 00:44:32 -0400

    feat: component for future table and other logic

---
+++ src/components/upload-studies/TableOfStudies.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { useTable, usePagination, useExpanded } from 'react-table';
+
+
+const TableOfStudies = () => {
+
+  return (
+    <p>tabla</p>
+  );
+}
+
+
+export default TableOfStudies;
