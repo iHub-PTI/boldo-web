78c0b21c33146497dcdbd161022ff444f343aa49
elias7991 <eliascaceres124@gmail.com>
2023-05-09 01:24:22 -0400

    feat: logic to handle visibility of the table

--- src/components/upload-studies/UploadStudies.tsx
+++ src/components/upload-studies/UploadStudies.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import SearchIcon from './SearchIcon';
 import PlusIcon from './PlusIcon';
 import BackIcon from './BackIcon';
+import TableOfStudies from './TableOfStudies';
 
 
 type Props = {
@@ -13,11 +14,18 @@ const UploadStudies = () => {
   const [searchValue, setSearchValue] = useState<string>('')
   const [searchError, setSearchError] = useState<boolean>(false)
   const [showNewStudyWithOutOrder, setShowNewStudyWithOutOrder] = useState<boolean>(true)
+  const [showTable, setShowTAble] = useState<boolean>(false)
   
   // This function handles the visibility of the back button
   const handleShowNewStudyWithOutOrder = () => {
     setShowNewStudyWithOutOrder(!showNewStudyWithOutOrder)
   }
+  
+  // this function handles the visibilitu of the table
+  const handleShowTable = (show: boolean) => {
+    setShowTAble(show)
+  }
+
 
   return (
     // container for all the upload studies section
@@ -27,7 +35,10 @@ const UploadStudies = () => {
         {/* this only show when new study is clicked */}
         <button 
           className={`flex flex-row ml-6 mt-2 w-full focus:outline-none ${showNewStudyWithOutOrder ? 'invisible' : 'visible'}`}
-          onClick={handleShowNewStudyWithOutOrder}
+          onClick={() => {
+            handleShowNewStudyWithOutOrder()
+            setShowTAble(true)
+          }}
         >
           <BackIcon />
           <p 
@@ -46,7 +57,10 @@ const UploadStudies = () => {
         {/* secondary header */}
         <p className='not-italic font-sans font-normal text-xl leading-6 m-6'>Busque una orden o adjunte un nuevo estudio</p>
         {/* bar for search studies for order number */}
-        <div className='flex flex-row pt-1 pb-1 w-2/3 rounded-lg hover:bg-gray-200 transition duration-300'>
+        <div 
+          className='flex flex-row pt-1 pb-1 w-2/3 rounded-lg hover:bg-gray-200 transition duration-300'
+          onClick={() => handleShowTable(true)}
+        >
           <SearchIcon />
           <input 
             id='searchStudy'
@@ -62,7 +76,10 @@ const UploadStudies = () => {
         { showNewStudyWithOutOrder &&
           <button 
             className='flex flex-row mt-4 mb-2 pt-1 pb-1 w-2/3 focus:outline-none rounded-lg hover:bg-gray-100 transition duration-300'
-            onClick={handleShowNewStudyWithOutOrder}
+            onClick={() => {
+              handleShowNewStudyWithOutOrder()
+              setShowTAble(false)
+            }}
           >
             <PlusIcon />
             <p 
@@ -72,6 +89,9 @@ const UploadStudies = () => {
               Adjuntar nuevo estudio sin orden
             </p>
           </button>
+        }
+        { showTable &&
+          <TableOfStudies />
         }
       </div>
       {/* footer */}
