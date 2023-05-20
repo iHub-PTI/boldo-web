86bf249e392c7f0aa5ac5cd23a8114550cf252e5
elias7991 <eliascaceres124@gmail.com>
2023-05-19 22:51:47 -0400

    refact: use new profile and date components

--- src/components/upload-studies/DetailPanel.tsx
+++ src/components/upload-studies/DetailPanel.tsx
@@ -2,14 +2,11 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import handleSendSentry from '../../util/Sentry/sentryHelper';
 import { ERROR_HEADERS } from '../../util/Sentry/errorHeaders';
-import { countDays } from '../../util/helpers'
 import { ReactComponent as SpinnerLoading } from "../../assets/spinner-loading.svg";
-import NoProfilePicture from '../icons/NoProfilePicture';
-import CalendarIcon from '../icons/upload-icons/CalendarIcon';
-import moment from 'moment';
 import Category from './Category';
 import Urgency from './Urgency';
-import DoctorName from './DoctorName';
+import DoctorProfile from './DoctorProfile';
+import DateSection from './DateSection';
 
 
 type Props = {
@@ -61,48 +58,9 @@ const DetailPanel = (props: Props) => {
                 {/* profile and date */}
                 <div className='flex flex-row justify-between'>
                   {/* profile */}
-                  <div className='flex flex-col space-y-2'>
-                    {/* text */}
-                    <p className='font-semibold text-xs not-italic leading-4 text-gray-500'>Orden</p>
-                    {/* photo and description */}
-                    <div className='flex flex-row space-x-4'>
-                      { orderStudy?.doctor?.photoUrl !== undefined
-                        ? <img
-                            src={orderStudy.doctor.photoUrl}
-                            alt='Foto de Perfil'
-                            className='flex-none border-1 border-white w-11 h-11 rounded-full object-cover'
-                          />
-                        : <NoProfilePicture className='bg-gray-200 rounded-full border-gray-200 border-1 w-11 h-11' />
-                      }
-                      {/* name and description */}
-                      <div className='flex flex-col'>
-                        {/* prefix and name */}
-                        <DoctorName doctor={orderStudy.doctor} className='font-normal text-base not-italic leading-6 text-gray-700' />
-                        {/* description */}
-                        <div className='flex flex-row items-center space-x-1'>
-                          {/* specialty */}
-                          <p className='font-normal text-xs not-italic leading-4 text-gray-600'>Cardiólogo</p>
-                          {/* circle */}
-                          <div className="h-2 w-2 rounded-full bg-gray-500"></div>
-                          {/* Hospital */}
-                          <p className='font-normal text-xs not-italic leading-4 text-gray-600'>Hospital Los Ángeles</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                  <DoctorProfile doctor={orderStudy?.doctor} />
                   {/* date section */}
-                  <div className='flex flex-col space-y-4'>
-                    <p className='font-semibold text-xs not-italic leading-4 text-gray-500'>Solicitado en fecha</p>
-                    <div className='flex flex-row items-center space-x-3'>
-                      {/* icon and date */}
-                      <div className='flex flex-row items-center space-x-2'>
-                        <CalendarIcon />
-                        <p className='font-normal text-sm not-italic leading-4'>{(moment(orderStudy?.authoredDate ?? new Date())).format('DD/MM/YYYY')}</p>
-                      </div>
-                      {/* count days ago */}
-                      <p className='font-normal text-sm not-italic leading-4 text-gray-500'>{countDays(orderStudy.authoredDate) ?? 'No fue posible calcular'}</p>
-                    </div>
-                  </div>
+                  <DateSection authoredDate={orderStudy?.authoredDate} />
                 </div>
                 {/* study order details */}
                 <div className='flex flex-col'>
