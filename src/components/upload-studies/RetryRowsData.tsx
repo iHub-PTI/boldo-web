e6018f3e8d0b68f0af681dfa8f544a4feff83e24
elias7991 <elias@elias>
2023-06-05 17:18:19 -0400

    feat: add logic to handle the click and some styles

--- src/components/upload-studies/RetryRowsData.tsx
+++ src/components/upload-studies/RetryRowsData.tsx
@@ -7,11 +7,24 @@ type Props = {
 const RetryRowsData = (props: Props) => {
   const {loadRef} = props
 
+  const handleClick = () => {
+    if (!loadRef) return
+    loadRef.current?.onQueryChange()
+  }
+
   return (
-    <div className='w-full h-full flex flex-col items-center justify-center'>
-      <p>Hubo un error al cargar los datos.</p>
-      <button>
-        <p>Reintentar</p>
+    <div className='w-full h-full flex flex-col space-y-3 items-center justify-center'>
+      <p className='not-italic font-semibold text-base leading-5'>Hubo un error al cargar los datos.</p>
+      <button
+        className='rounded-lg focus:outline-none'
+        style={{
+          backgroundColor: '#27BEC2'
+        }}
+        onClick={handleClick}
+      >
+        <p className='px-1 py-2 not-italic font-semibold text-base leading-5 text-white'>
+          Reintentar
+        </p>
       </button>
     </div>
   )
