ac7833919477506e95a645fae45c9ecb9edd91f9
Sebastian <sebasrival@gmail.com>
2024-03-11 17:39:56 -0300

    feat: se agrega servicio para enviar el reporte del paciente

--- src/services/services.ts
+++ src/services/services.ts
@@ -4,3 +4,23 @@ export function getStudyOrdersByEncounter(encounterId: string, patientId: string
   const url = `/profile/doctor/patient/${patientId}/encounter/${encounterId}/studyOrders`
   return axios.get<Boldo.OrderStudy[]>(url)
 }
+
+interface ReasonPatient {
+  id: number
+  patientId: string
+  encounterId: string
+  reporterUsername: string
+  commentReason: string
+  creationDateTime: string
+  notificationEmailSent: boolean
+}
+
+
+export function sendReportPatiend(patientId:string, encounterId:string, commentReason:string) {
+  const url = `/profile/doctor/patient/${patientId}/report`
+  return axios.post<ReasonPatient>(url,{
+    patientId,
+    encounterId,
+    commentReason
+  })
+}
